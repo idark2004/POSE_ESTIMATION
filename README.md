7f02bd149400514ded6902e528ed7297df94178a
Ho Huu Phat <phathuuho@gmail.com>
2022-12-30 16:27:31 +0700

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Run this line in command prompt to instal all packages
+
+`pip freeze > requirements.txt`
+
+## To run program
+
+`python -u "path_to_the_directory\pose.py`
--- pose.py
+++ pose.py
@@ -21,23 +21,21 @@ with mp_pose.Pose(
         if not ret:
             print("Ignoring No Video in Camera frame")
             continue
-        
-        
-        
+
 #----------------------------------------
 # Drawing
         frame.flags.writeable = False
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert to RGB
         results = pose.process(frame)
-        
+
         frame.flags.writeable = True
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) # convert to BGR
-        
+
         mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        
+
         cv2.imshow("Pose output", cv2.flip(frame, 1))
-        
+
         if cv2.waitKey(5) & 0xFF == ord('q'): # hit q to stop
             break
-        
+
 cap.release()
